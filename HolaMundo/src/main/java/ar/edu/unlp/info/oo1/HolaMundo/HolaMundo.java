ca965f736c8c71d00ef822992cfefd118dcec8d5
Marcos Matias <marcosmatias086@gmail.com>
2023-05-22 20:22:44 -0300

    Primer commit

---
+++ HolaMundo/src/main/java/ar/edu/unlp/info/oo1/HolaMundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package ar.edu.unlp.info.oo1.HolaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		int num1=10;
+		int num2=40;
+		System.out.println("La suma de los numeros es  : " + (num1+num2));
+	}
+
+}
